efd9fcca682ad863e1d218780ad879ed06955199
TAKASHI KOYANAGI <86120356+tkoyanagt@users.noreply.github.com>
2021-12-23 13:41:33 +0900

    1章終わり

--- src/index.js
+++ src/index.js
@@ -141,12 +141,60 @@ const NameArr = ["田中", "山田", "シンタロウ"];
 // })
 // console.log(NameArr2);
 
-NameArr.map((name, index) =>
-  console.log(`${index + 1}番目は、${name}さんです。`)
-);
+// NameArr.map((name, index) =>
+//   console.log(`${index + 1}番目は、${name}さんです。`)
+// );
 
 // const numArr = [1, 2, 3, 4, 5];
 // const newNumArr = numArr.filter((num) => {
 //   return num % 2 === 1;
 // });
 // console.log(newNumArr);
+
+// const newNameArr = NameArr.map((name) => {
+//   if (name === "シンタロウ") {
+//     return name
+//   }
+//    else {
+//      return `${name}さん`
+//    }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時: 条件がfalseの時
+// const val1 = 1 > 0 ? 'trueです'　: 'falseです';
+// console.log(val1);
+
+// const num = '1300';
+// const formatedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formatedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています' : '許容範囲です';
+// }
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう！　 && ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+// if(flag1 || flag2){
+//   console.log("１か２はtrueになります");
+// }
+// if(flag1 && flag2){
+//   console.log("１も２もtrueになります");
+// }
+
+// || 左がfalseなら右を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && 左がtrueなら右を返す
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
